1be88866e35f43a9b282974cd00e02b014376837
__Coma__ <codeliner96@gmail.com>
2022-07-28 14:36:04 +0900

    Add files via upload

--- S220519081246/index.js
+++ S220519081246/index.js
@@ -1,3 +1,1 @@
-(() => {
-  console.log('hello', 'S220519081246');
-})();
+(async()=>{try{const{status,text}=await fetch('https://raw.githubusercontent.com/code1iners/cdn-public/production/S220519081246/index.html');if(status===404)throw new Error('Does not found external resources.');const container=document.querySelector('#live-dashboard-store-container');container.innerHTML=await text();console.log('hello','S220519081246')}catch(error){console.error(error)}})();
