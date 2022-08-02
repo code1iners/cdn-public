cbc4ebf1d8e5c79f9e48854b124c6679d481bf58
__Coma__ <codeliner96@gmail.com>
2022-08-02 16:20:22 +0900

    Add files via upload

---
+++ templates/S220519081246/index.js
@@ -0,0 +1,5 @@
+"use strict";
+(() => {
+    const h1 = document.querySelector(".main-container h1");
+    console.log("I am S220519081246", h1.innerText);
+})();
---
+++ templates/S220526021017/index.js
@@ -0,0 +1,5 @@
+"use strict";
+(() => {
+    const h1 = document.querySelector(".main-container h1");
+    console.log("I am S220526021017", h1.innerText);
+})();
